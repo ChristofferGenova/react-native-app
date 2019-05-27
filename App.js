2c046f4a46a7cc09ddc550ddec1e5a46f8678e6e
christoffergenova <christoffergenova1@gmail.com>
2019-05-27 14:37:40 -0400

    17 - Criando componente App

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.f40}>
+          App! HIHIHI
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  f40: {
+    fontSize: 40
+  }
+})
