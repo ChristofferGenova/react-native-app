29d2b0eab01d3c6d90cc2768c64f8ff0a0162c5b
christoffergenova <christoffergenova1@gmail.com>
2019-05-27 16:15:23 -0400

    21 - Renderizacao condicional com operador ternario

---
+++ src/componentes/ParImpar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Padrao from '../estilo/Padrao';
+
+function parOuImpar(num){
+    //Operador ternário
+    const resultado = num % 2 == 0 ? 'Par' : 'Impar'
+    return <Text style={Padrao.ex}>{resultado}</Text>
+}
+
+export default props => {
+    return(
+        <View>
+            {parOuImpar(props.numero)}
+            {/* {
+                //Operador ternário
+                props.numero % 2 == 0
+                ? <Text style={Padrao.ex}>Par</Text> //Se for par renderiza um texto escriot Par
+                : <Text style={Padrao.ex}>Impar</Text> //Senão renderiza um texto escrito Impar
+            } */}
+        </View>
+    )
+    
+}
