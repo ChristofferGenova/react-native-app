e2185401ad6dcfad7fa0e3e4f27862c31b03783c
christoffergenova <christoffergenova1@gmail.com>
2019-05-28 09:39:13 -0400

    24 - contador

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { createDrawerNavigator } from 'react-navigation'
 
-import Simples from "./componentes/Simples";
-import ParImpar from "./componentes/ParImpar";
-import { Inverter, MegaSena } from "./componentes/Multi";
+import Simples from "./componentes/Simples"
+import ParImpar from "./componentes/ParImpar"
+import { Inverter, MegaSena } from "./componentes/Multi"
+import Contador from './componentes/Contador'
 
 export default createDrawerNavigator({
+    Contador: {
+        screen: () => <Contador></Contador>
+    },
     MegaSena: {
         screen: () => <MegaSena numeros={8}></MegaSena>,
         navigationOptions: { title: 'Mega Sena' }
